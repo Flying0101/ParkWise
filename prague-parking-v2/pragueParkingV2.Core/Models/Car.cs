726c73e6c8e76bc0969d1ac81c2c424b5e9c2305
Stephano <stephano@live.se>
2024-12-21 18:02:30 +0100

    feat: add car model, data structure

---
+++ prague-parking-v2/pragueParkingV2.Core/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace prague_parking_v2.pragueParkingV2.Core.Models
+{
+    // Represents a Car vehicle, inheriting from the Vehicle base class.
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber) : base(registrationNumber) { }
+        public override int Size => 4;
+    }
+}
