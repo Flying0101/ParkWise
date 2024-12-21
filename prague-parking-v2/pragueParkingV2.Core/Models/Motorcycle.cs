d841e5cda5490cc88d686a0c8d81e446240bf778
Stephano <stephano@live.se>
2024-12-21 18:02:48 +0100

    feat: add mc model, data structure

---
+++ prague-parking-v2/pragueParkingV2.Core/Models/Motorcycle.cs
@@ -0,0 +1,10 @@
+﻿namespace prague_parking_v2.pragueParkingV2.Core.Models
+{
+
+    // Represents a motorcycle vehicle, inheriting from the Vehicle base class.
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle(string registrationNumber) : base(registrationNumber) { }
+        public override int Size => 2;
+    }
+}
